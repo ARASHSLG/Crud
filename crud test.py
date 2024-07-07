5bf45aef664f1988b95d7f9eb7c17707f1f1dbba
arash <yekadam17@gmail.com>
2024-07-07 13:47:01 +0330

    test basic of project

---
+++ crud test.py
@@ -0,0 +1,26 @@
+# from tkinter import *
+# from pymongo import MongoClient
+# client = MongoClient("localhost", 27017)
+# db = client['Crud']
+# persons = db['Person']
+# win = Tk()
+# win.geometry("800x200")
+# win.title('Crud Project')
+# win.iconbitmap(r"C:\Users\arash\Desktop\Coding\Python\crud\icons\Screenshot 2024-05-23 013137.png")
+#
+# win.mainloop()
+
+from tkinter import *
+from pymongo import MongoClient
+
+win = Tk()
+win.geometry("800x600")
+win.title("Crud Project")
+name = Entry(cursor="", width=25, fg="black", background="#eeeee4")
+name.place(x=100 , y=100)
+win.iconbitmap(r"icons/king_icon-icons.com_69359.ico")
+win.configure(background="#22011c", height=100, width=100, borderwidth=1, border=2)
+
+# btn
+# registerBtn=
+win.mainloop()
