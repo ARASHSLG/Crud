58d0a9bc46c6002434698ad0590c72df5ee4b8e8
arash <yekadam17@gmail.com>
2024-07-10 11:11:07 +0330

    change some one line funcs to lambda

--- crud test.py
+++ crud test.py
@@ -13,20 +13,12 @@ import tkinter
 from tkinter import ttk
 from tkinter import *
 from pymongo import MongoClient
-# server set
+# database set
 Client = MongoClient("localhost", 27017)
 db = Client["Crud"]
 persons = db["Persons"]
 
-# defs
-
-
-def change_button_style(event):
-    registerBtn.configure(bg="#033cef", fg="white")
-
-
-def default_btnstyles(event):
-    registerBtn.configure(fg="black", background="#eeeee4")
+# functions
 
 
 def read(person):
@@ -37,16 +29,23 @@ def onclick_read(event):
     person = {"name": name.get(),
               "family": family.get(),
               "age": age.get(),
-              "major" : major.get()}
+              "major": major.get()}
     read(person)
+    alldata = alldata_read()
+    for data in alldata:
+        add_data_totable(data)
+
+
+def add_data_totable(person):
+    table.insert("", "end", values=[person['name'],person['family'], person['age'], person['major']])
 
 
 def alldata_read():
     alldata = persons.find()
-    print(alldata[0])
-
+    return alldata
 
 # main task
+
 
 win = Tk()
 win.geometry("950x600")
@@ -54,7 +53,7 @@ win.title("Crud Project")
 win.iconbitmap(r"icons/king_icon-icons.com_69359.ico")
 win.configure(background="#22011c", height=100, width=100, borderwidth=1, border=2)
 
-# txt
+# texts
 name = Entry(win, bd=5, font=("arial", 15), justify="center", width=25, fg="black", bg="#eeeee4")
 name.place(x=100, y=100)
 
@@ -67,27 +66,30 @@ age.place(x=100, y=220)
 major = Entry(win, bd=5, font=("arial", 15), justify="center", width=25, fg="black", background="#eeeee4")
 major.place(x=100, y=280)
 
-# lbl
-welcomelabel = tkinter.Label(win, text="Welcome To Crud MiniProject", font=("new times roman", 25), fg="white", bg="#22011c")
+# labels
+welcomelabel = tkinter.Label(win, text="Welcome To Crud MiniProject",
+                             font=("new times roman", 25), fg="white", bg="#22011c")
 welcomelabel.place(x=300, y=0)
 
-nmlabel = tkinter.Label(win, text="Name : ", font=("arial", 15), width=5, bd=5, fg="white", bg="#22011c")
+nmlabel = tkinter.Label(win, text="Name : ", font=("times", 15), width=5, bd=5, fg="white", bg="#22011c")
 nmlabel.place(x=25, y=100)
 
-fmlabel = tkinter.Label(win, text="Family : ", font=("arial", 15), width=6, bd=5, fg="white", bg="#22011c")
+fmlabel = tkinter.Label(win, text="Family : ", font=("times", 15), width=6, bd=5, fg="white", bg="#22011c")
 fmlabel.place(x=20, y=160)
 
-agelabel = tkinter.Label(win, text="age : ", font=("arial", 15), width=5, bd=5, fg="white", bg="#22011c")
+agelabel = tkinter.Label(win, text="age : ", font=("times", 15), width=5, bd=5, fg="white", bg="#22011c")
 agelabel.place(x=25, y=220)
 
-majorlabel = tkinter.Label(win, text="Major : ", font=("arial", 15), width=5, bd=5, fg="white", bg="#22011c")
+majorlabel = tkinter.Label(win, text="Major : ", font=("times", 15), width=5, bd=5, fg="white", bg="#22011c")
 majorlabel.place(x=25, y=280)
 
-# btn
-registerBtn = Button(win, cursor="hand2", text='submit', bd=5, font=("arial", 15), width=12, fg="black", background="#eeeee4")
+# button
+registerBtn = Button(win, cursor="hand2", text='submit', bd=5,
+                     font=("arial", 15), width=12, fg="black", background="#eeeee4")
 registerBtn.place(x=165, y=340)
-registerBtn.bind("<Enter>", change_button_style)
-registerBtn.bind("<Leave>", default_btnstyles)
+registerBtn.bind("<Enter>", lambda event: registerBtn.configure(bg="#033cef", fg="white"))
+registerBtn.bind("<Leave>", lambda event: registerBtn.configure(fg="black", background="#eeeee4"))
+registerBtn.bind("<Button-1>", onclick_read)
 
 
 # table
